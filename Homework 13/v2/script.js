236633a456e811bd0e644b294dfd52ee4b3adfb5
Victor Adamovich <ndptrv@yandex.by>
2021-05-13 02:39:07 +0300

    go slip ))))

--- Homework 13/v2/script.js
+++ Homework 13/v2/script.js
@@ -1,4 +1,9 @@
-function addRow(id) {
+// Добавление блоков
+var add = document.getElementById('add'),
+    block = document.getElementsByTagName('td'),
+    input = document.getElementsByClassName('hide');
+
+add.addEventListener('click', function addRow(id) {
     //Даже не знаю как сделать более красиво ((
     var tbody = document.getElementById('tbody'),
         row = document.createElement('tr'),
@@ -10,8 +15,37 @@ function addRow(id) {
     row.prepend(td2);
     row.prepend(td3);
     tbody.prepend(row);
+});
+
+// Работа с input
+
+function newInput(e, item, active) {
+    if (e.addEventListener)
+        e.addEventListener(item, active);
+    else
+        e.attachEvent(item, active);
 }
 
+var mainBlock = document.getElementById('tbody');
 
-var input = document.querySelector('.hide'),
-    td = document.getElementById('td');
+newInput(mainBlock, 'mouseup', function (e) {
+    if (e.target.tagName.toLowerCase() == 'td') {
+        input.value = block.textContent;
+        e.target.appendChild(input);
+        input.classList.remove('hide');
+        input.focus();
+    }
+});
+
+input.addEventListener('blur', function (e) {
+    e.target.parentElement.textContent = input.value;
+    input.classList.add('hide');
+    document.body.appendChild(input);
+    console.log(e.target);
+});
+
+document.addEventListener('keypress', function(e) {
+    if(e.key == 'Enter') {
+        input.classList.add('hidden');
+    }
+});
