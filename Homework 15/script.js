8e1b5703349b1ca7bb4811aee03dedf362a8ae77
Victor Adamovich <ndptrv@yandex.by>
2021-05-19 11:10:54 +0300

    add valid

--- Homework 15/script.js
+++ Homework 15/script.js
@@ -5,48 +5,46 @@ document.getElementById('block').addEventListener('keyup', function () {
     var x = document.getElementById('x').value,
         y = document.getElementById('y').value;
 
-    if (x > 0 && y > 0) {
-        button.removeAttribute('disabled')
+    if ((x > 0) && (y > 0)) {
+        button.removeAttribute('disabled');
     }
-})
+});
 
-//Добовление таблицы
+//Добовление таблицы и alert
 button.addEventListener('click', function () {
-        var x = document.getElementById('x').value,
-            y = document.getElementById('y').value;
+    var x = document.getElementById('x').value,
+        y = document.getElementById('y').value;
 
-        var t = document.getElementById('tbody');
+     if ((x > 10) || (y >10)){
+            alert('Мы поддерживам любыче числа, но в задаче ограничение до 10 (: ')
+         return;
+        };
 
-        while (t.firstChild) {
-            t.removeChild(t.firstChild)
-        }
-        ;
+    var t = document.getElementById('tbody');
 
-        for (var i = 0; i < y; i++) {
-            var newTr = document.createElement('TR');
-            t.appendChild(newTr)
-            for (var j = 0; j < x; j++) {
-                newTr.insertAdjacentHTML(
-                    'afterbegin',
-                    '<td></td>'
-                );
-            }
+    while (t.firstChild) {
+        t.removeChild(t.firstChild);
+    }
+    for (var i = 0; i < y; i++) {
+        var newTr = document.createElement('TR');
+        t.appendChild(newTr);
+        for (var j = 0; j < x; j++) {
+            newTr.insertAdjacentHTML('afterbegin', '<td></td>');
         }
     }
-);
+});
 
 //Переключатель
 var tgl = document.getElementById('table');
 
 tgl.addEventListener('click', function () {
-        if (tgl.className == 'even') {
-            tgl.classList.remove('even')
-            tgl.classList.add('odd')
-            return
-        } else {
-            tgl.classList.remove('odd');
-            tgl.classList.add('even');
-            return
-        }
+    if (tgl.className == 'even') {
+        tgl.classList.remove('even');
+        tgl.classList.add('odd');
+        return;
+    } else {
+        tgl.classList.remove('odd');
+        tgl.classList.add('even');
+        return;
     }
-)
+});
