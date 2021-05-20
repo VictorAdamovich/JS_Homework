826c3d70ff00dfad1e7523ea2c2d154087f6a307
Victor Adamovich <ndptrv@yandex.by>
2021-05-20 11:30:56 +0300

    optimize

--- Homework 15/script.js
+++ Homework 15/script.js
@@ -19,7 +19,7 @@ button.addEventListener('click', function () {
     if ((x > 10) || (y > 10)) {
         alert('Мы поддерживам любыче числа, но в задаче ограничение до 10 (: ')
         return;
-    };
+    }
 
     var t = document.getElementById('tbody');
 
@@ -40,13 +40,11 @@ button.addEventListener('click', function () {
 var tgl = document.getElementById('table');
 
 tgl.addEventListener('click', function () {
-    if (tgl.className == 'even') {
+    if (tgl.className === 'even') {
         tgl.classList.remove('even');
         tgl.classList.add('odd');
-        return;
     } else {
         tgl.classList.remove('odd');
         tgl.classList.add('even');
-        return;
     }
 });
