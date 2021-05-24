e424d1ef328bbc677e27179fa9253a72da9579f5
AdamovichV <77265974+AdamovichV@users.noreply.github.com>
2021-05-24 13:55:19 +0300

    bug fix

--- Homework 16/script.js
+++ Homework 16/script.js
@@ -1,6 +1,6 @@
 var xhr = new XMLHttpRequest();
 var arr;
-var container = document.getElementById('container')
+var container = document.getElementById('container');
 
 //Получение JSON
 document.getElementById('button').addEventListener('click', function sendXhr() {
@@ -9,12 +9,12 @@ document.getElementById('button').addEventListener('click', function sendXhr() {
     xhr.onload = function () {
         var status = +String(this.status)[0];
         if (status === 2) {
-            xhr = JSON.parse(xhr.responseText)
-            arr = xhr.data
+            xhr = JSON.parse(xhr.responseText);
+            arr = xhr.data;
             //Создание блока навигации
             var createNavi = document.createElement('div');
             createNavi.id = 'navi';
-            container.appendChild(createNavi)
+            container.appendChild(createNavi);
 
             //Создание блока карточки
             var createUserInfo = document.createElement('div');
@@ -23,71 +23,60 @@ document.getElementById('button').addEventListener('click', function sendXhr() {
             createNaviInfo();
             createProfileCar(arr[0]);
 
-
             //Создание блоков Navi
             function createNaviInfo() {
                 var navi = document.getElementById('navi');
                 for (var i = 0; i < arr.length; i++) {
-                    var profile = document.createElement('DIV')
+                    var profile = document.createElement('DIV');
                     profile.id = i;
-                    profile.className = 'user__profile'
+                    profile.className = 'user__profile';
                     profile.innerHTML = 'Пользователь ' + i;
-                    navi.appendChild(profile)
+                    navi.appendChild(profile);
                 }
             }
-
 
             document.getElementById('navi').addEventListener('click', function (e) {
                 if (e.target.className === 'user__profile') {
                     var userId = e.target.id;
-                    var deleteId = document.getElementById('user_info')
+                    var deleteId = document.getElementById('user_info');
                     while (deleteId.firstChild) {
                         deleteId.removeChild(deleteId.firstChild);
                     }
                     var user = arr[userId];
-                    createProfileCar(user)
+                    createProfileCar(user);
                 }
-            })
+            });
 
-//Наполнение карточки
+            //Наполнение карточки
             function createProfileCar(e) {
-                var addProfileInfo = e
-                var userInfo = document.getElementById('user_info')
+                var addProfileInfo = e;
+                var userInfo = document.getElementById('user_info');
                 var firsBlock = document.createElement('div'),
                     secondBlock = document.createElement('div');
                 userInfo.appendChild(firsBlock);
                 userInfo.appendChild(secondBlock);
 
-
                 //Add img
-                var img = document.createElement("IMG");
+                var img = document.createElement('IMG');
                 img.src = addProfileInfo.avatar;
                 firsBlock.appendChild(img);
 
                 //Add First name
                 var firstName = document.createElement('P');
-                firstName.textContent = 'FirstName: ' + addProfileInfo.first_name
+                firstName.textContent = 'FirstName: ' + addProfileInfo.first_name;
                 secondBlock.appendChild(firstName);
                 //Add Last name
                 var lastName = document.createElement('P');
-                lastName.textContent = 'FirstName: ' + addProfileInfo.last_name
+                lastName.textContent = 'FirstName: ' + addProfileInfo.last_name;
                 secondBlock.appendChild(lastName);
 
                 //Add email
-                var email = document.createElement('P')
-                email.textContent = 'Email: ' + addProfileInfo.email
+                var email = document.createElement('P');
+                email.textContent = 'Email: ' + addProfileInfo.email;
                 secondBlock.appendChild(email);
             }
-
         } else {
             container.insertAdjacentHTML('afterend', '<h1>Опа, ошибочка вышла</h1>');
         }
     };
-})
-
-
-
-
-
-
-
+});
