edf58d8a4bbb0332cda869c82cd08396236df946
Victor Adamovich <ndptrv@yandex.by>
2021-05-24 10:26:40 +0300

    add hw 16

---
+++ Homework 16/script.js
@@ -0,0 +1,68 @@
+var xhr = new XMLHttpRequest();
+var arr;
+
+//Получение JSON
+document.getElementById('button').addEventListener('click', function sendXhr() {
+    xhr.open("GET", "https://reqres.in/api/users?page=2", true);
+    xhr.send();
+    xhr.onload = function () {
+        localStorage.users= xhr
+
+        xhr = JSON.parse(xhr.responseText)
+        arr = xhr.data
+        createNavi()
+        createProfileCar()
+    };
+})
+
+document.getElementById('navi').addEventListener('click',function (event){
+    if(event.target.className == 'user__profile' ){
+        var user = event.target.id
+        var test = arr[user]
+        var t = document.getElementById('user_info')
+        while (t.firstChild) {
+            t.removeChild(t.firstChild);
+        }
+        createProfileCar(test)
+    }
+})
+
+//Создание блоков Navi
+function createNavi() {
+    var navi = document.getElementById('navi');
+    for (var i = 0; i < arr.length; i++) {
+        var profile = document.createElement('DIV')
+        profile.id = i;
+        profile.className = 'user__profile'
+        profile.innerHTML = 'Пользователь ' + i;
+        navi.appendChild(profile)
+    }
+}
+
+//Создание карточки
+function createProfileCar(test) {
+    var test2 = test
+    var userInfo = document.getElementById('user_info')
+
+    //Add img
+    var img = document.createElement("IMG");
+    img.src = test2.avatar;
+    userInfo.appendChild(img);
+
+    //Add First name
+    var firstName = document.createElement('P');
+    firstName.textContent = 'FirstName: ' +test2.first_name
+    userInfo.appendChild(firstName);
+    //Add Last name
+    var lastName = document.createElement('P');
+    lastName.textContent = 'FirstName: ' +test2.last_name
+    userInfo.appendChild(lastName);
+
+    //Add email
+    var email = document.createElement('P')
+    email.textContent = 'Email: ' + test2.email
+    userInfo.appendChild(email);
+}
+
+
+
